0217345d5d88657ddb67a99dbeae2d475230ec6f
Abdul Wahab <abdul.wahab394.aw@gmail.com>
2023-10-13 23:30:58 +0500

    promotional categories done

--- components/sections/PromotionalCategoriesSection.tsx
+++ components/sections/PromotionalCategoriesSection.tsx
@@ -5,8 +5,8 @@ import PromotionalCategoryCard from '../cards/PromotionalCategoryCard';
 import {useAppSelector} from '@store/hooks';
 import {selectPromotionalCategories} from '@store/slices/category/catgory.slice';
 
-const PromotionalCategoriesSection= () => {
-  const promotionalCategories= useAppSelector(selectPromotionalCategories)
+const PromotionalCategoriesSection = () => {
+  const promotionalCategories = useAppSelector(selectPromotionalCategories);
 
   return (
     <>
@@ -17,7 +17,7 @@ const PromotionalCategoriesSection= () => {
               POPULAR PROMOTIONAL PRODUCT CATEGORIES
             </h2>
             <ul className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-6 sm:gap-20 md:gap-16 xl:gap-8 2xl:gap-20">
-              {promotionalCategories?.map((category) => (
+              {promotionalCategories?.map(category => (
                 <li key={category.id} className="mt-16 sm:mt-0">
                   <PromotionalCategoryCard category={category} />
                 </li>
