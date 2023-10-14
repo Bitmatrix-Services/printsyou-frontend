702fac4800875d35cc0c767eaa63ce7e90f14e1a
Abdul Wahab <abdul.wahab394.aw@gmail.com>
2023-10-15 01:48:31 +0500

    fall back src added for image

--- components/cards/PromotionalCategoryCard.tsx
+++ components/cards/PromotionalCategoryCard.tsx
@@ -5,6 +5,7 @@ import sanitize from 'sanitize-html';
 
 import {ShoppingBagIcon} from '@heroicons/react/24/outline';
 import {Category} from '@store/slices/category/category';
+import ImageWithFallback from '@components/ImageWithFallback';
 
 interface CategoryCardProps {
   category: Category;
@@ -18,15 +19,16 @@ const PromotionalCategoryCard: FC<CategoryCardProps> = ({category}) => {
     >
       <div className="pt-14 pb-2 sm:py-10 flex flex-col 2xl:flex-row items-center px-6 2xl:px-0 gap-6 xl:gap-0">
         <figure className="block absolute -top-16 left-1/2 -translate-x-1/2 sm:top-0 sm:left-0 sm:translate-x-0 sm:relative min-w-[7rem] w-28 h-28 sm:min-w-[13.125rem] sm:w-[13.125rem] sm:h-[13.125rem] 2xl:scale-105 2xl:-translate-x-11">
-          <Image
+          <ImageWithFallback
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             className="object-contain"
             fill
             src={
-              category?.imageUrl && category.imageUrl
-                ? `${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${category.imageUrl}`
-                : '/assets/logo.png'
+              category?.imageUrl &&
+              category.imageUrl &&
+              `${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${category.imageUrl}`
             }
+            fallbackSrc={'/assets/logo.png'}
             alt="category"
           />
         </figure>
