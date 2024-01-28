0f6ce8cad4989c4ed21ba422466929a2b00cc376
awaistariq <awais.tariqq@gmail.com>
2024-01-27 20:55:54 -0600

    query param encode for product page

--- pages/products/[...uniqueProductName].tsx
+++ pages/products/[...uniqueProductName].tsx
@@ -332,7 +332,7 @@ export const getServerSideProps = async (
 
     if (Array.isArray(uniqueProductName)) {
       const {data} = await http.get(
-        `product?uProductName=${uniqueProductName.join('/')}`
+        `product?uProductName=${encodeURIComponent(uniqueProductName.join('/'))}`
       );
       product = data.payload;
     }
