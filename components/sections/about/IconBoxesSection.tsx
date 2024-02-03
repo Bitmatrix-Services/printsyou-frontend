397784f3cc06a939bff8b2bad08d18a994056892
Abdul Wahab <abdul.wahab394.aw@gmail.com>
2024-02-04 01:00:31 +0500

    about us icons section updated

--- components/sections/about/IconBoxesSection.tsx
+++ components/sections/about/IconBoxesSection.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Container from '../../globals/Container';
-import AboutPageServicesCard from '@components/cards/AboutPageServicesCard';
+import Image from 'next/image';
 
 const identityitems = [
   {
-    image: '/assets/art.svg',
+    image: '/assets/art-service.svg',
     heading: 'Art Services',
     text: 'Simply provide us with your materials. Our skilled art team will adapt it into the ideal format for imprinting on your selected products.'
   },
   {
-    image: '/assets/rush.svg',
+    image: '/assets/rush-service.svg',
     heading: 'Rush Service',
     text: 'Many of our promotional items are eligible for 3-day or even 1-day Rush Service without any extra cost.'
   },
   {
-    image: '/assets/email.svg',
+    image: '/assets/email-service.svg',
     heading: 'E-Mail Order Updates',
     text: `We'll keep you updated on your order's status, from the entry date to the due date, through production, and finally, with an email including your UPS tracking numbers. Rest assured, you won't need to follow up on your order's progress.`
   },
   {
-    image: '/assets/idea.svg',
+    image: '/assets/idea-theme-center.svg',
     heading: 'Idea Theme Center',
     text: `Our seasoned sales team offers personalized Idea Presentations free of charge. Simply share your campaign's theme and budget with us. Same business day, we'll deliver a vibrant, full-color presentation tailored to your event or target audience, featuring unique promotional products.`
   }
@@ -31,16 +31,28 @@ const IconBoxesSection = () => {
       <Container>
         <div className="lg:flex justify-center items-center gap-12 text-center lg:text-left">
           <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
-            {identityitems.map((items, index) => {
-              return (
-                <AboutPageServicesCard
-                  key={index}
-                  image={items.image}
-                  heading={items.heading}
-                  text={items.text}
-                />
-              );
-            })}
+            {identityitems.map(item => (
+              <div
+                key={item.heading}
+                className="flex flex-col justify-center items-center text-center pt-2 pb-2 px-7 "
+              >
+                <div className="h-40 w-40 min-w-[10rem] relative  mb-5">
+                  <Image
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw "
+                    fill
+                    className="object-contain"
+                    src={item.image}
+                    alt="logos"
+                  />
+                </div>
+                <h6 className="mt-4 font-bold text-base mb-5">
+                  {item.heading}
+                </h6>
+                <div className="text-mute3 font-medium text-[14px] leading-6 px-3">
+                  {item.text}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </Container>
