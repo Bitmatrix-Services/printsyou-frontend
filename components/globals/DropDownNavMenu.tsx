b615cf7d2c7f645c794762448a54909852033e5f
Abdul Wahab <abdul.wahab394.aw@gmail.com>
2024-02-09 18:03:57 +0500

    search and drop down changes

--- components/globals/DropDownNavMenu.tsx
+++ components/globals/DropDownNavMenu.tsx
@@ -5,6 +5,8 @@ import sanitizeHtml from 'sanitize-html';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Container from './Container';
 import {Category} from '@store/slices/category/category';
+import Image from 'next/image';
+import ImageWithFallback from '@components/ImageWithFallback';
 
 interface DropDownNavMenuProps {
   subCatList: Category[];
@@ -18,7 +20,7 @@ export const DropDownNavMenu: FC<DropDownNavMenuProps> = ({
   subCatList
 }) => {
   const [showList, setShowList] = useState<boolean>(false);
-
+  const [catImage, setCatImage] = useState<string>('');
   return (
     <div
       className={`megamenu ${className} ${showList && 'show'} hidden md:block`}
@@ -36,25 +38,53 @@ export const DropDownNavMenu: FC<DropDownNavMenuProps> = ({
       </button>
       <div className="megamenu-inner">
         <Container>
-          <ul className="menu-link columns-5 space-y-4">
-            {subCatList?.map(category => (
-              <li key={category.id} onClick={() => setShowList(false)}>
-                <Link
-                  className="flex text-sm text-mute hover:text-secondary-500 transition-all duration-150 group"
-                  href={`/${category.uniqueCategoryName}`}
+          <div className="flex gap-4">
+            <ul className="menu-link mr-auto columns-4 space-y-4">
+              {subCatList?.map(category => (
+                <li
+                  key={category.id}
+                  onClick={() => setShowList(false)}
+                  onMouseEnter={() => setCatImage(category.imageUrl)}
+                  onMouseLeave={() => setCatImage('')}
                 >
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: sanitizeHtml(category.categoryName)
-                    }}
-                  ></span>
-                  <span className="ml-1 transition-all duration-150 opacity-0 group-hover:opacity-100">
-                    <TrendingFlatIcon />
-                  </span>
-                </Link>
-              </li>
-            ))}
-          </ul>
+                  <Link
+                    className="flex text-sm text-mute hover:text-secondary-500 transition-all duration-150 group"
+                    href={`/${category.uniqueCategoryName}`}
+                  >
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: sanitizeHtml(category.categoryName)
+                      }}
+                    ></span>
+                    <span className="ml-1 transition-all duration-150 opacity-0 group-hover:opacity-100">
+                      <TrendingFlatIcon />
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+
+            <div className="category-image">
+              <Image
+                height={400}
+                width={400}
+                src="/assets/image-1.png"
+                alt="category"
+              />
+            </div>
+
+            {/* {catImage && (
+              <div className='category-image'>
+                
+                     {/* <ImageWithFallback
+                  height={400}
+                  width={400}
+                  // src={catImage}
+                  alt="category"
+                /> 
+              </div>
+            )}*/}
+          </div>
         </Container>
       </div>
     </div>
--- components/inputs/SearchBar.tsx
+++ components/inputs/SearchBar.tsx
@@ -172,7 +172,7 @@ const SearchBar = () => {
       </div>
 
       {searchQuery && showResults === 'data found' && (
-        <div className="search-menu absolute z-20 w-full overflow-auto top-14 left-0 bg-white border border-[#ddd] shadow-md p-2 rounded-b-md">
+        <div className="search-menu absolute z-20 w-[77%] overflow-auto top-14 left-[1.3rem] bg-white border border-[#ddd] shadow-md p-2 rounded-b-md">
           <div className="space-y-3">
             {searchResult?.categories && (
               <fieldset>
