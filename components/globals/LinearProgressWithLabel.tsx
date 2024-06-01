fac9958a7e16ddd1e1cfe00ccf1d2c7c3f0f7a30
Abdul Wahab <abdul.wahab394.aw@gmail.com>
2024-06-01 22:24:26 +0500

    upload image progress bar added

---
+++ components/globals/LinearProgressWithLabel.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {FC} from 'react';
+
+interface ILinearProgressWithLabel {
+  progress: number;
+}
+
+const LinearProgressWithLabel: FC<ILinearProgressWithLabel> = ({progress}) => {
+  return (
+      <div className="w-full bg-gray-200 rounded-full h-4 my-2">
+          <div
+              className="bg-primary-500 h-4  rounded-full flex items-center justify-center text-white font-bold"
+              style={{width: `${progress}%`}}
+          >
+              {progress}%
+          </div>
+      </div>
+  );
+};
+
+export default LinearProgressWithLabel;
