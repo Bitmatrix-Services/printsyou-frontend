8e6b76a0d91bcd49c4bd731f1b1bcb8217e80af8
Muhammad Usama <the.mgi@outlook.com>
2024-07-01 02:48:37 +0500

    Update axios.service.ts

--- services/axios.service.ts
+++ services/axios.service.ts
@@ -6,6 +6,7 @@ const config = getConfig();
 const DOMAIN_BASE_URL =
   config.publicRuntimeConfig.DOMAIN_BASE_URL || 'https://identity.the-mgi.com';
 
+
 export const http = axios.create({
   baseURL: DOMAIN_BASE_URL
 });
