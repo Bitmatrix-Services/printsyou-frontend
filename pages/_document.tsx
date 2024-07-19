0c60f03d4c92ea438c30c008a259c4b38ec31717
Muhammad Usama <the.mgi@outlook.com>
2024-07-20 01:51:01 +0500

    google analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Document, {Head, Html, Main, NextScript} from 'next/document';
+import Script from 'next/script';
+
+class AppDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-P54L18DJYN"
+          />
+
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+   window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-P54L18DJYN');
+`
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default AppDocument;
