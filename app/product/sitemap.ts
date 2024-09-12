c28970cc28740365b41377ecc0e127cf94ab9a7c
Abdul Wahab <abdul.wahab394.aw@gmail.com>
2024-09-12 23:05:18 +0500

    static and product sitemap done

--- app/product/sitemap.ts
+++ app/product/sitemap.ts
@@ -3,9 +3,12 @@ import {MetadataRoute} from 'next';
 
 const feUrl = process.env.FE_URL;
 
+interface ISitemapProduct {
+  loc: string;
+}
+
 export async function generateSitemaps() {
   const productChunks: number = await getSitemapStuff('product-chunks');
-  console.log('productChunks', productChunks);
   return Array.from({length: productChunks}).map((_, index) => {
     return {
       id: index
@@ -14,11 +17,11 @@ export async function generateSitemaps() {
 }
 
 export default async function sitemap({id}: {id: number}): Promise<MetadataRoute.Sitemap> {
-  const products = await getSitemapStuff('product', {chunk: id.toString()});
+  const products: ISitemapProduct[] = await getSitemapStuff('product', {chunk: id.toString()});
 
   return products.map(product => ({
-    url: `${feUrl}/product/${product.uniqueProductName}`,
-    lastModified: product.date,
+    url: `${feUrl}product/${product.loc}`,
+    lastModified: new Date(),
     changeFrequency: 'monthly',
     priority: 0.5
   }));
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,12 +3,12 @@ import {getSitemapStuff} from '@utils/utils';
 
 const feUrl = process.env.FE_URL;
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap | any> {
   const productChunks: number = await getSitemapStuff('product-chunks');
 
   const productSitemap = Array.from({length: productChunks}).map((_, index) => {
     return {
-      url: `${feUrl}sitemap_products.xml/${index}`,
+      url: `${feUrl}product/sitemap.xml/${index}`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5
