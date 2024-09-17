94d17cab70cd1347ccf38251d76778b397adee2f
Muhammad Usama <the.mgi@outlook.com>
2024-09-17 21:40:45 +0500

    updated footer and store ld

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from 'next';
-import {PropsWithChildren} from 'react';
+import React, {PropsWithChildren} from 'react';
 import {ReactQueryClientProvider} from './query-client-provider';
 import 'swiper/css';
 import 'swiper/css/thumbs';
@@ -44,6 +44,70 @@ export default async function RootLayout({children}: PropsWithChildren) {
         <CSPostHogProvider>
           <html lang="en">
             <Head>
+              <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                  __html: JSON.stringify({
+                    '@context': 'http://schema.org',
+                    '@type': 'LocalBusiness',
+                    name: 'Prints You',
+                    description:
+                      'Discover top-quality promotional products. Perfect for trade shows, conventions or office swag. Elevate your brand with unique promotional products today!',
+                    telephone: '877-934-1874',
+                    email: 'info@printsyou.com',
+                    address: {
+                      '@type': 'PostalAddress',
+                      addressLocality: 'Rowlett',
+                      addressRegion: 'TX',
+                      streetAddress: '8602 Royal Star Road',
+                      postalCode: '75089',
+                      addressCountry: 'US'
+                    },
+                    sameAs: [
+                      ['https://www.facebook.com/PrintsYouPromotional', 'https://www.linkedin.com/company/printsyou']
+                    ],
+                    url: 'https://printsyou.com',
+                    image: 'https://printsyou.com/assets/logo-full.png'
+                  })
+                }}
+              />
+              <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                  __html: JSON.stringify({
+                    '@context': 'https://schema.org',
+                    '@type': 'Store',
+                    image: [
+                      "https://printsyou.com/assets/logo-full.png"
+                    ],
+                    name: "Prints You Custom Promotional Products",
+                    address: {
+                      '@type': 'PostalAddress',
+                      addressLocality: 'Rowlett',
+                      addressRegion: 'TX',
+                      streetAddress: '8602 Royal Star Road',
+                      postalCode: '75089',
+                      addressCountry: 'US'
+                    },
+                    priceRange: '$$$',
+                    telephone: '+18779341874',
+                    openingHoursSpecification: [
+                      {
+                        '@type': 'OpeningHoursSpecification',
+                        dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+                        opens: '08:00',
+                        closes: '19:00'
+                      },
+                      {
+                        '@type': 'OpeningHoursSpecification',
+                        dayOfWeek: 'Sunday',
+                        opens: '10:00',
+                        closes: '18:00'
+                      }
+                    ],
+                  })
+                }}
+              />
               <link href="https://fonts.cdnfonts.com/css/graphik-trial" rel="stylesheet" />
             </Head>
             <body className="overflow-x-hidden" style={{fontFamily: 'Graphik Trial, sans-serif'}}>
