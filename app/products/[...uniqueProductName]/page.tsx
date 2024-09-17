5c1548c6ca843a4d4898d27d3d35932eb1f33ef5
Muhammad Usama <the.mgi@outlook.com>
2024-09-17 21:26:39 +0500

    product ld

--- app/products/[...uniqueProductName]/page.tsx
+++ app/products/[...uniqueProductName]/page.tsx
@@ -2,6 +2,8 @@ import {getProductDetailsByUniqueName} from '@components/home/product/product-ap
 import {ProductDetails} from '@components/home/product/product-details.component';
 import {Product} from '@components/home/product/product.types';
 import {metaConstants} from '@utils/constants';
+import Head from 'next/head';
+import React from 'react';
 
 const ProductsPage = async ({params}: {params: {uniqueProductName: string[]}}) => {
   const response = await getProductDetailsByUniqueName(params.uniqueProductName.join('/'));
@@ -9,7 +11,34 @@ const ProductsPage = async ({params}: {params: {uniqueProductName: string[]}}) =
 
   if (response?.payload) product = response.payload;
 
-  return <ProductDetails product={product} />;
+  return (
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org/',
+              '@type': 'Product',
+              name: product?.productName,
+              image: (product?.productImages ?? []).map(item => `${process.env.ASSETS_SERVER_URL}${item.imageUrl}`),
+              description: (product?.description ?? '').replace(/<[^>]+>/g, ''),
+              sku: product?.sku,
+              offers: {
+                '@type': 'Offer',
+                url: `${process.env.FE_URL}${product?.uniqueProductName}`,
+                itemCondition: 'https://schema.org/NewCondition',
+                availability: 'https://schema.org/InStock',
+                priceCurrency: 'USD',
+                price: [...(product?.priceGrids ?? [])].sort((a, b) => a.countFrom - b.countFrom).pop()?.price
+              }
+            })
+          }}
+        />
+      </Head>
+      <ProductDetails product={product} />
+    </>
+  );
 };
 
 export default ProductsPage;
--- components/home/product/product-details.component.tsx
+++ components/home/product/product-details.component.tsx
@@ -26,29 +26,6 @@ export const ProductDetails: FC<IProductDetails> = ({product}) => {
 
   return (
     <>
-      <Head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              '@context': 'https://schema.org/',
-              '@type': 'Product',
-              name: product.productName,
-              image: (product.productImages ?? []).map(item => `${process.env.ASSETS_SERVER_URL}${item.imageUrl}`),
-              description: (product.description ?? '').replace(/<[^>]+>/g, ''),
-              sku: product.sku,
-              offers: {
-                '@type': 'Offer',
-                url: `${process.env.FE_URL}${product.uniqueProductName}`,
-                itemCondition: 'https://schema.org/NewCondition',
-                availability: 'https://schema.org/InStock',
-                priceCurrency: 'USD',
-                price: [...(product.priceGrids ?? [])].sort((a, b) => a.countFrom - b.countFrom).pop()?.price
-              }
-            })
-          }}
-        />
-      </Head>
       {product ? (
         <>
           <Breadcrumb prefixTitle="Promotional Products" list={product.crumbs} />
