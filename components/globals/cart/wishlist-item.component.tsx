07c0c5ddccd34e37402e48020a3d78286b76f684
Ali Mazhar <alimaza4444@gmail.com>
2024-09-22 17:26:49 +0500

    Add to cart from wishlist button

--- components/globals/cart/wishlist-item.component.tsx
+++ components/globals/cart/wishlist-item.component.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import {IoCloseCircle} from 'react-icons/io5';
 import {useAppDispatch} from 'store/hooks';
 import {removeFromWishlist} from 'store/slices/wishlist/wishlist.slice';
+import {setCartStateForModal} from 'store/slices/cart/cart.slice';
 
 export const WishListItem = ({product}: any) => {
   const dispatch = useAppDispatch();
@@ -30,7 +31,22 @@ export const WishListItem = ({product}: any) => {
             <h6 className="text-green-600 font-bold  text-base uppercase"> in stock</h6>
             <div className="flex gap-4">
               <div className="text-center text-xs py-1 px-3 rounded-full cursor-pointer text-secondary-500 bg-secondary-100/25 hover:text-white hover:bg-primary-500">
-                <div className="capitalize font-bold">add to cart</div>
+                <button
+                  onClick={e => {
+                    dispatch(
+                      setCartStateForModal({
+                        selectedProduct: structuredClone(product),
+                        open: true,
+                        selectedItem: null,
+                        cartMode: 'new'
+                      })
+                    );
+                    e.preventDefault();
+                  }}
+                  className="capitalize font-bold"
+                >
+                  add to cart
+                </button>
               </div>
               <IoCloseCircle
                 className="h-6 w-6 text-mute cursor-pointer hover:text-primary-700"
