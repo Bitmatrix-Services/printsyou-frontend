293659b2294f7593fbad673bb9f59533c64a1111
Muhammad Usama <the.mgi@outlook.com>
2024-09-25 20:32:13 +0500

    gtm stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,63 +15,60 @@ import {metaConstants} from '@utils/constants';
 import {NotificationComponent} from '@components/notification/notification.component';
 import {CSPostHogProvider} from './provider';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
-    metadataBase: new URL(process.env.FE_URL as string),
+  metadataBase: new URL(process.env.FE_URL as string),
+  title: metaConstants.SITE_NAME,
+  description: metaConstants.DESCRIPTION,
+  openGraph: {
+    images: '/assets/logo-full.png',
     title: metaConstants.SITE_NAME,
-    description: metaConstants.DESCRIPTION,
-    openGraph: {
-        images: '/assets/logo-full.png',
-        title: metaConstants.SITE_NAME,
-        description: metaConstants.DESCRIPTION
-    }
+    description: metaConstants.DESCRIPTION
+  }
 };
 
 const AddToCartModalClientSide = dynamic(
-    () => import('./../components/globals/cart/add-to-cart-modal.component').then(file => file.AddToCartModal),
-    {
-        ssr: false,
-        loading: () => null
-    }
+  () => import('./../components/globals/cart/add-to-cart-modal.component').then(file => file.AddToCartModal),
+  {
+    ssr: false,
+    loading: () => null
+  }
 );
 
 export default async function RootLayout({children}: PropsWithChildren) {
-    const categoriesData = await getAllCategories();
+  const categoriesData = await getAllCategories();
 
-    return (
-        <ReduxProvider>
-            <ReactQueryClientProvider>
-                <CSPostHogProvider>
-                    <html lang="en">
-                    <Head>
-                        <link href="https://fonts.cdnfonts.com/css/graphik-trial" rel="stylesheet"/>
-                        <!-- Google tag (gtag.js) -->
-                        <Script
-                            strategy="beforeInteractive"
-                            src="https://www.googletagmanager.com/gtag/js?id=AW-16709127988"
-                        />
-                        <Script
-                            id="google-tag-manager"
-                            strategy="afterInteractive"
-                            dangerouslySetInnerHTML={{
-                                __html: `window.dataLayer = window.dataLayer || [];
+  return (
+    <ReduxProvider>
+      <ReactQueryClientProvider>
+        <CSPostHogProvider>
+          <html lang="en">
+            <Head>
+              <link href="https://fonts.cdnfonts.com/css/graphik-trial" rel="stylesheet" />
+              <Script strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=AW-16709127988" />
+              <Script
+                id="google-tag-manager"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];
                                 function gtag(){dataLayer.push(arguments);}
                                 gtag('js', new Date());
                                 
                                 gtag('config', 'AW-16709127988');`
-                            }}
-                        />
-                    </Head>
-                    <body className="overflow-x-hidden" style={{fontFamily: 'Graphik Trial, sans-serif'}}>
-                    <NotificationComponent/>
-                    <Header categories={categoriesData.payload}/>
-                    {children}
-                    <Footer categories={categoriesData.payload.slice(0, 6)}/>
-                    <AddToCartModalClientSide/>
-                    </body>
-                    </html>
-                </CSPostHogProvider>
-            </ReactQueryClientProvider>
-        </ReduxProvider>
-    );
+                }}
+              />
+            </Head>
+            <body className="overflow-x-hidden" style={{fontFamily: 'Graphik Trial, sans-serif'}}>
+              <NotificationComponent />
+              <Header categories={categoriesData.payload} />
+              {children}
+              <Footer categories={categoriesData.payload.slice(0, 6)} />
+              <AddToCartModalClientSide />
+            </body>
+          </html>
+        </CSPostHogProvider>
+      </ReactQueryClientProvider>
+    </ReduxProvider>
+  );
 }
