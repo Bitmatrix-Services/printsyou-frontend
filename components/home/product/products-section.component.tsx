6f5fd3e4f31adc0163db3dcb92d1785ebd444506
Abdul Wahab <abdul.wahab394.aw@gmail.com>
2024-09-26 15:41:45 +0500

    catrgories query params added

--- components/home/product/products-section.component.tsx
+++ components/home/product/products-section.component.tsx
@@ -1,12 +1,14 @@
 'use client';
 import React, {FC, useEffect, useState} from 'react';
-import {useSearchParams} from 'next/navigation';
+import {usePathname, useRouter, useSearchParams} from 'next/navigation';
 import axios from 'axios';
 import PaginationHeader from '@components/globals/pagination-header';
 import {EnclosureProduct} from '@components/home/product/product.types';
 import {ProductRoutes} from '@utils/routes/be-routes';
 import {CircularLoader} from '@components/globals/circular-loader.component';
 import {SearchProductCard} from '@components/search/search-product-card';
+import {IQueryParams} from '@components/search/search-results-section';
+import {allowableSearchParams} from '@utils/constants';
 
 interface ProductsSectionProps {
   categoryId: string;
@@ -17,30 +19,30 @@ interface ProductsSectionProps {
 
 export const ProductsSection: FC<ProductsSectionProps> = ({categoryId, categoryName, prefix, suffix}) => {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
   const minPrice = searchParams.get('minPrice');
   const maxPrice = searchParams.get('maxPrice');
   const [productsByCategory, setProductsByCategory] = useState<EnclosureProduct[]>([]);
 
-  const [page, setPage] = useState<number>(1);
-  const [pageSize, setPageSize] = useState<number>(20);
   const [totalPages, setTotalPages] = useState<number>(1);
-  const [sort, setSort] = useState('priceLowToHigh');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isPageLoading, setIsPageLoading] = useState<boolean>(true);
 
+  const {page, filter, size}: IQueryParams = Object.fromEntries(
+    ['page', 'filter', 'size'].map(param => [param, searchParams.get(param)])
+  );
+
   useEffect(() => {
     if (categoryId) getProductByCategory();
-  }, [categoryId, pageSize, sort, minPrice, maxPrice, page]);
+  }, [categoryId, size, filter, page]);
 
   const getProductByCategory = async () => {
     try {
       setIsLoading(true);
-      let query = `${process.env.NEXT_PUBLIC_API_BASE_URL}${ProductRoutes.ProductByCategoryId}/${categoryId}?&size=${pageSize}&filter=${sort}&minPrice=0&maxPrice=10000`;
+      let query = `${process.env.NEXT_PUBLIC_API_BASE_URL}${ProductRoutes.ProductByCategoryId}/${categoryId}?page=${page ?? 1}&size=${size ?? 20}&filter=${filter ?? 'priceLowToHigh'}&minPrice=0&maxPrice=10000`;
       if (maxPrice && minPrice) {
         query += `&minPrice=${minPrice}&maxPrice=${maxPrice}`;
-      }
-      if (page) {
-        query += `&page=${page}`;
       }
       const {data} = await axios.get(query);
 
@@ -55,6 +57,23 @@ export const ProductsSection: FC<ProductsSectionProps> = ({categoryId, categoryN
       setIsLoading(false);
     }
   };
+  const handleQueryUpdate = (value: string | number, queryName: string) => {
+    const currentQuery = getUpdatedQueryParams();
+    let updatedQuery = {...currentQuery, [queryName]: value};
+    router.push(`${pathname}?${new URLSearchParams(updatedQuery)}`);
+  };
+
+  const getUpdatedQueryParams = (): Record<string, any> => {
+    let updatedQuery: Record<string, any> = {};
+
+    searchParams.forEach((value, key) => {
+      if (~allowableSearchParams.indexOf(key)) {
+        updatedQuery[key] = value;
+      }
+    });
+
+    return updatedQuery;
+  };
 
   return (
     <section className="bg-white pt-8 md:pt-10 lg:pt-16">
@@ -67,13 +86,13 @@ export const ProductsSection: FC<ProductsSectionProps> = ({categoryId, categoryN
       ) : null}
       {productsByCategory?.length > 0 && !isPageLoading && (
         <PaginationHeader
-          pageNumber={page || 1}
-          setPageNumber={setPage}
-          pageSize={pageSize}
-          setPageSize={setPageSize}
+          pageNumber={(page && parseInt(page)) || 1}
+          setPageNumber={(value: string | number) => handleQueryUpdate(value, 'page')}
+          pageSize={(size && parseInt(size)) || 20}
+          setPageSize={(value: string | number) => handleQueryUpdate(value, 'size')}
           totalPages={totalPages}
-          sort={sort}
-          setSort={value => setSort(value)}
+          sort={filter || 'priceLowToHigh'}
+          setSort={(value: string) => handleQueryUpdate(value, 'filter')}
         />
       )}
 
@@ -90,13 +109,13 @@ export const ProductsSection: FC<ProductsSectionProps> = ({categoryId, categoryN
       )}
       {productsByCategory?.length > 0 && !isPageLoading && (
         <PaginationHeader
-          pageNumber={page || 1}
-          setPageNumber={setPage}
-          pageSize={pageSize}
-          setPageSize={setPageSize}
+          pageNumber={(page && parseInt(page)) || 1}
+          setPageNumber={(value: string | number) => handleQueryUpdate(value, 'page')}
+          pageSize={(size && parseInt(size)) || 20}
+          setPageSize={(value: string | number) => handleQueryUpdate(value, 'size')}
           totalPages={totalPages}
-          sort={sort}
-          setSort={value => setSort(value)}
+          sort={filter || 'priceLowToHigh'}
+          setSort={(value: string) => handleQueryUpdate(value, 'filter')}
         />
       )}
 
