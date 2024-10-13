f754b293bc2ca13638a99b0c131d60a11d99f832
Abdul <abdul.wahab394.aw@gmail.com>
2024-10-14 01:36:08 +0500

    search page title undefined fixed

--- app/search-results/page.tsx
+++ app/search-results/page.tsx
@@ -1,9 +1,5 @@
-import {Metadata} from 'next';
-import {metaConstants} from '@utils/constants';
 import {SearchResult} from '@components/search-result.components';
 import dynamic from 'next/dynamic';
-import {getCategoryDetailsByUniqueName} from '@components/home/category/category.apis';
-import {Category} from '@components/home/home.types';
 
 const SearchResultPage = () => {
   return <SearchResult />;
@@ -22,8 +18,16 @@ export async function generateMetadata(queryParams: {searchParams: any}) {
     canonicalURL = `${canonicalURL}?page=${currentPage}`;
   }
 
+  const searchKeyword = queryParams.searchParams.keywords
+    ? queryParams.searchParams.keywords
+    : queryParams.searchParams.tag === 'newAndExclusive'
+      ? 'New and Exclusive'
+      : queryParams.searchParams.tag === 'featured'
+        ? 'Just a Buck'
+        : 'Most Popular';
+
   return {
-    title: `Search for ${queryParams.searchParams.searchQuery} showing page ${currentPage} - PrintsYou`,
+    title: `Search for ${searchKeyword} showing page ${currentPage} - PrintsYou`,
     description: `Find the perfect promotional products tailored to your needs. Explore a wide range of options from your search results to elevate your brand's marketing and outreach efforts.`,
     alternates: {
       canonical: canonicalURL
