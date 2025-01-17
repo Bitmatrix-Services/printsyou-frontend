f92cef3586f16bc60570e1aa08ab417cd72aaa08
awaistariq <awais.tariqq@gmail.com>
2025-01-16 18:48:58 -0600

    adding order-now into robot.txt

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/more-info', 'https://api.printsyou.com']
+      disallow: ['/more-info', '/order-now', 'https://api.printsyou.com', 'https://chatwoot.printsyou.com/']
     },
     sitemap: 'https://printsyou.com/api/sitemap.xml'
   };
