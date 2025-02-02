9f281a925e5558fab74211769223a15aeafec0f7
the-mgi <abdul.wahab394.aw@gmail.com>
2025-02-03 03:05:26 +0500

    dynamic banner added

--- components/home/banner/grid-banner.component.tsx
+++ components/home/banner/grid-banner.component.tsx
@@ -5,22 +5,22 @@ import Link from 'next/link';
 import {BannerList} from '@components/home/home.types';
 
 interface IGridBannerComponentProps {
-  banner: BannerList;
+  banner: BannerList[];
 }
 
 export const GridBannerComponent: FC<IGridBannerComponentProps> = ({banner}) => {
   return (
     <div className="w-full max-w-[120rem] mx-auto md:px-2 lg:px-8 2xl:px-0 relative">
-      <div key={banner.id} className="grid sm:grid-rows-2 lg:grid-rows-2 xl:grid-rows-1 xl:grid-cols-8">
+      <div className="grid sm:grid-rows-2 lg:grid-rows-2 xl:grid-rows-1 xl:grid-cols-8">
         {/*  tagline section */}
         <div className="banner-text-section font-century-gothic xl:col-span-2">
           <div className="flex flex-col justify-center items-start xl:max-w-[32rem] text-start text-white px-5 lg:px-8 py-8 h-full font-normal text-4xl lg:text-5xl tracking-wide leading-extra-loose lg:leading-extra-loose">
-            <h1>Carry in Style: The Perfect Tote for Every Occasion</h1>
+            <h1 dangerouslySetInnerHTML={{__html: banner[0].heading}}></h1>
             <div className="mt-4 w-full">
               <Link
-                href={`/categories/bags-duffels-amp-accessories/tote-bags-nylon-amp-polyester`}
+                href={`/categories/${banner[0].bannerCategory.ucategoryName}`}
                 className="block xl:inline-block text-lg text-[#005c90] align-middle text-center bg-white px-4 lg:px-8 py-3 tablet:py-1 md:py-1 xl:py-3 font-normal font-century-gothic hover:bg-blue-100"
-                dangerouslySetInnerHTML={{__html: 'Buy Tote Bags and Gifts'}}
+                dangerouslySetInnerHTML={{__html: banner[0].tagLines}}
               ></Link>
             </div>
           </div>
@@ -30,22 +30,22 @@ export const GridBannerComponent: FC<IGridBannerComponentProps> = ({banner}) => 
         <div className="relative min-h-[150px] md:min-h-[170px] lg:min-h-full w-full xl:col-span-3">
           <Image
             className="object-cover"
-            src={`https://printsyouassets.s3.amazonaws.com/static-assets/banners/FullToteBag.png`}
-            alt="Image 2"
+            src={`${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${banner[1].bannerUrl}`}
+            alt={banner[1].heading}
             fill
           />
         </div>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-1 sm:grid-rows-2 tablet:grid-rows-1 md:grid-rows-2 lg:grid-rows-1 px-4 lg:px-6 md:px-4 xl:pl-4 xl:pr-0  gap-4 lg:gap-32 xl:gap-4 mt-4 md:pt-5 tablet:mt-5 lg:mt-4 xl:mt-0 xl:pt-0 xl:col-span-3">
           <ShortImageTitleSection
             textColor={'white'}
-            heading={'Made in USA'}
-            ucategoryName={'made-in-usa'}
-            bannerUrl={'static-assets/banners/made-in-usa-banner.jpg'}
+            heading={banner[2].heading}
+            ucategoryName={banner[2].bannerCategory.ucategoryName}
+            bannerUrl={banner[2].bannerUrl}
           />
           <ShortImageTitleSection
-            heading={'Pet Products'}
-            ucategoryName={'pet-products'}
-            bannerUrl={'static-assets/banners/pet-supplies.jpg'}
+            heading={banner[3].heading}
+            ucategoryName={banner[3].bannerCategory.ucategoryName}
+            bannerUrl={banner[3].bannerUrl}
           />
         </div>
       </div>
@@ -54,10 +54,10 @@ export const GridBannerComponent: FC<IGridBannerComponentProps> = ({banner}) => 
 };
 
 interface IShortImageTitleSection {
-  ucategoryName: String;
-  heading: String;
-  bannerUrl: String;
-  textColor?: String;
+  ucategoryName: string;
+  heading: string;
+  bannerUrl: string;
+  textColor?: string;
 }
 
 const ShortImageTitleSection: FC<IShortImageTitleSection> = ({ucategoryName, heading, bannerUrl, textColor}) => {
@@ -66,13 +66,13 @@ const ShortImageTitleSection: FC<IShortImageTitleSection> = ({ucategoryName, hea
       <Image
         className="object-cover tablet:object-cover md:object-cover"
         src={`${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${bannerUrl}`}
-        alt="Image 4"
+        alt={heading}
         fill
       />
       <div
         className={`absolute font-century-gothic ${textColor === 'white' ? 'text-white' : 'text-[#3aa4dc]'} bottom-4 ml-6 mb-10 text-left font-normal`}
       >
-        <div className="text-2xl mb-1" dangerouslySetInnerHTML={{__html: heading}}></div>
+        <div className="text-2xl mb-1 capitalize" dangerouslySetInnerHTML={{__html: heading}}></div>
         <Link href={`/categories/${ucategoryName}`} className="text-lg underline ">
           Shop Now
         </Link>
--- components/home/hero-section.component.tsx
+++ components/home/hero-section.component.tsx
@@ -1,18 +1,58 @@
 'use client';
-
-import React, {FC} from 'react';
-import {SwiperSlider} from '@components/globals/swiper.component';
+import React, {FC, useMemo, useRef} from 'react';
 import {BannerList} from '@components/home/home.types';
+import {Swiper, SwiperRef, SwiperSlide} from 'swiper/react';
+import {Pagination} from 'swiper/modules';
 import {GridBannerComponent} from '@components/home/banner/grid-banner.component';
+import {FullBannerComponent} from '@components/home/banner/full-banner.component';
 
 interface IHeroSection {
   bannersList: BannerList[];
 }
 
 export const HeroSection: FC<IHeroSection> = ({bannersList = []}) => {
+  const sliderRef = useRef<SwiperRef>(null);
+  const groupAndSortBanners = (data: BannerList[]): Record<string, BannerList[]> => {
+    const grouped = data.reduce((acc: Record<string, BannerList[]>, item) => {
+      const type = item.type;
+      acc[type] = acc[type] || [];
+      acc[type].push(item);
+      return acc;
+    }, {});
+
+    Object.keys(grouped).forEach(type => {
+      grouped[type].sort((a, b) => a.sequenceNumber - b.sequenceNumber);
+    });
+
+    return grouped;
+  };
+  const typeSortedBanners = useMemo(() => {
+    return groupAndSortBanners(bannersList);
+  }, [bannersList]);
+
   return (
-    <SwiperSlider dataList={bannersList.sort((a, b) => a.sequenceNumber - b.sequenceNumber)}>
-      {banner => <GridBannerComponent banner={banner} />}
-    </SwiperSlider>
+    <section className="bg-grey">
+      <div className="relative">
+        <Swiper
+          ref={sliderRef}
+          loop
+          modules={[Pagination]}
+          spaceBetween={10}
+          slidesPerView={1}
+          pagination={{clickable: true}}
+          className="hero-swiper"
+        >
+          {Object.entries(typeSortedBanners).map(([bannerType, items]) => (
+            <SwiperSlide key={bannerType}>
+              {bannerType.includes('grid') && items.length === 4 ? (
+                <GridBannerComponent banner={items} />
+              ) : bannerType.includes('full') && items.length === 1 ? (
+                <FullBannerComponent banner={items[0]} />
+              ) : null}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
   );
 };
