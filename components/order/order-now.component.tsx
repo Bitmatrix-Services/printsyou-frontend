211c71f7925460b725a56b08c9bbb2d697851905
the-mgi <the.mgi@outlook.com>
2025-02-03 01:50:32 +0500

    deployment

--- components/order/order-now.component.tsx
+++ components/order/order-now.component.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React, {ChangeEvent, FC, Fragment, useEffect, useMemo, useState} from 'react';
-import Link from 'next/link';
 import {notFound, useRouter} from 'next/navigation';
 import axios, {AxiosResponse} from 'axios';
 import {OrderNowFormSchemaType, orderNowSchema} from '@utils/validation-schemas';
