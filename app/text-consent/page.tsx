54aea0c9c7229e05238650b3093fac8fa5d8c2fd
Muhammad Usama <the.mgi@outlook.com>
2025-02-03 00:04:56 +0500

    textual consent

--- app/text-consent/page.tsx
+++ app/text-consent/page.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import {Metadata} from 'next';
 import {Container} from '@components/globals/container.component';
 import {metaConstants} from '@utils/constants';
-import React from 'react';
+import Link from 'next/link';
 
 const TextConsent = () => {
   return (
@@ -13,17 +14,17 @@ const TextConsent = () => {
             <div className="pb-2">
               <h3 className="text-xl mt-2 mb-1 font-bold text-primary-500">Text Notification Consent</h3>
               <p className="font-normal text-mute2 text-base leading-[30px]">
-                At <a href="/">PrintsYou.com</a>, we offer text notifications to keep you informed about your orders,
-                special promotions, and updates. Signing up for text notifications is completely optional, and you can
-                opt out at any time.
+                At <Link href="/">PrintsYou.com</Link>, we offer text notifications to keep you informed about your
+                orders, special promotions, and updates. Signing up for text notifications is completely optional, and
+                you can opt out at any time.
               </p>
             </div>
             <div className="pb-2">
               <h3 className="text-xl mt-2 mb-1 font-bold text-primary-500">How It Works</h3>
               <p className="font-normal text-mute2 text-base leading-[30px]">
-                By opting in, you agree to receive automated text messages from <a href="/">PrintsYou.com</a> related to
-                your order status, promotions, and important updates. Message and data rates may apply. Your consent to
-                receive these messages is not required to make a purchase.
+                By opting in, you agree to receive automated text messages from <Link href="/">PrintsYou.com</Link>{' '}
+                related to your order status, promotions, and important updates. Message and data rates may apply. Your
+                consent to receive these messages is not required to make a purchase.
               </p>
             </div>
             <div className="pb-2">
@@ -37,8 +38,8 @@ const TextConsent = () => {
                     Reply <strong>STOP</strong> to any message you receive.
                   </li>
                   <li>
-                    Email us at <a href="mailto:support@printsyou.com">support@printsyou.com</a> with your request to
-                    unsubscribe.
+                    Email us at <Link href="mailto:support@printsyou.com">support@printsyou.com</Link> with your request
+                    to unsubscribe.
                   </li>
                 </ul>
                 <br />
