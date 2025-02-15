b4499095f6c048e3f74e27254592339a56bece73
the-mgi <the.mgi@outlook.com>
2025-02-15 23:57:50 +0500

    prettier

--- components/notification/notification.component.tsx
+++ components/notification/notification.component.tsx
@@ -1,31 +1,31 @@
 'use client';
-import React, { FC, useEffect, useState } from 'react';
-import { Notification } from '@utils/util-types';
-import { getAllNotifications } from '@components/notification/notification-apis';
-import { sortSortable } from '@utils/utils';
+import React, {FC, useEffect, useState} from 'react';
+import {Notification} from '@utils/util-types';
+import {getAllNotifications} from '@components/notification/notification-apis';
+import {sortSortable} from '@utils/utils';
 
 export const NotificationComponent: FC = () => {
-    const [notifications, setNotifications] = useState<Notification[]>([]);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
 
-    useEffect(() => {
-        const fetchNotifications = async () => {
-            try {
-                const notis = await getAllNotifications();
-                setNotifications(sortSortable(notis));
-            } catch (error) {
-                console.error('Failed to fetch notifications', error);
-            }
-        };
-        fetchNotifications();
-    }, []);
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const notis = await getAllNotifications();
+        setNotifications(sortSortable(notis));
+      } catch (error) {
+        console.error('Failed to fetch notifications', error);
+      }
+    };
+    fetchNotifications();
+  }, []);
 
-    return notifications.map(({ id, backgroundColor, notificationMessage }) => (
-        <div
-            key={id}
-            className="w-full flex justify-center items-center min-h-[30px] py-1 bg-opacity-80"
-            style={{ backgroundColor }}
-        >
-            <span dangerouslySetInnerHTML={{ __html: notificationMessage }} />
-        </div>
-    ));
+  return notifications.map(({id, backgroundColor, notificationMessage}) => (
+    <div
+      key={id}
+      className="w-full flex justify-center items-center min-h-[30px] py-1 bg-opacity-80"
+      style={{backgroundColor}}
+    >
+      <span dangerouslySetInnerHTML={{__html: notificationMessage}} />
+    </div>
+  ));
 };
