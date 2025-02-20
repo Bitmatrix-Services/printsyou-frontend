dddd7a9bfd5d3d5890347195b876e1937d07cc94
Abdul Hannan Malik <ahmalik99@gmail.com>
2025-02-20 19:45:03 +0500

    Fixes: Fixed banner sizing in Laptop-XL, Tablet and Phone screens

--- components/home/banner/grid-banner.component.tsx
+++ components/home/banner/grid-banner.component.tsx
@@ -10,10 +10,10 @@ interface IGridBannerComponentProps {
 
 export const GridBannerComponent: FC<IGridBannerComponentProps> = ({banner}) => {
   return (
-    <div className="w-full max-w-[120rem] mx-auto md:px-2 lg:px-16 2xl:px-0 relative">
-      <div className="grid sm:grid-rows-1 lg:grid-rows-2 xl:grid-rows-1 xl:grid-cols-8">
+    <div className="w-full max-w-[120rem] mx-auto md:px-2 lg:px-8 2xl:px-0 relative">
+      <div className="grid sm:grid-rows-1 lg:grid-rows-2 xl:grid-rows-1 xl:grid-cols-9">
         {/*  tagline section */}
-        <div className="banner-text-section font-century-gothic xl:col-span-3 h-full md:h-[400px] lg:h-full">
+        <div className="banner-text-section font-century-gothic xl:col-span-3 h-full md:h-[16rem] lg:h-[25rem] w-full">
           <div className="flex flex-col justify-center items-start xl:max-w-[43rem] text-start text-white px-5 lg:px-8 py-8 h-full font-normal text-4xl lg:text-5xl tracking-wide leading-extra-loose lg:leading-extra-loose">
             <h1 dangerouslySetInnerHTML={{__html: banner[0].heading}}></h1>
             <div className="mt-4 w-full">
@@ -27,25 +27,18 @@ export const GridBannerComponent: FC<IGridBannerComponentProps> = ({banner}) => 
         </div>
 
         {/*  center image section */}
-        <div className="relative min-h-[170px] md:min-h-[170px] lg:min-h-full w-full xl:col-span-3">
+        <div className="relative min-h-[10.625rem] md:h-[20rem] lg:h-[25rem] w-full xl:col-span-4">
           <Image
-            className="object-cover w-full h-full"
+            className="object-fill w-full h-full"
             src={`${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${banner[1].bannerUrl}`}
             alt={banner[1].heading}
             priority
             width={752}
-            height={240}
-            // sizes="(max-width: 768px) 393px, (max-width: 1024px) 752px, 516px"
-            //@ts-ignore
-            srcSet={`
-                ${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${banner[1].bannerUrl} 393w,
-                ${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${banner[1].bannerUrl} 752w,
-                ${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${banner[1].bannerUrl} 516w
-            `}
+            height={516}
           />
         </div>
 
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-1 sm:grid-rows-2 tablet:grid-rows-1 md:grid-rows-2 lg:grid-rows-1 px-4 lg:px-6 md:px-4 xl:pl-4 xl:pr-0  gap-4 lg:gap-32 xl:gap-4 mt-4 md:pt-5 tablet:mt-5 lg:mt-4 xl:mt-0 xl:pt-0 xl:col-span-2">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-1 sm:grid-rows-2 tablet:grid-rows-1 md:grid-rows-1 lg:grid-rows-1 px-4 lg:px-6 md:px-4 xl:pl-4 xl:pr-0  gap-4 lg:gap-32 xl:gap-4 mt-4 md:pt-0 tablet:mt-5 lg:mt-4 xl:mt-0 xl:pt-0 xl:col-span-2">
           <ShortImageTitleSection
             textColor={'white'}
             heading={banner[2].heading}
@@ -72,21 +65,14 @@ interface IShortImageTitleSection {
 
 const ShortImageTitleSection: FC<IShortImageTitleSection> = ({ucategoryName, heading, bannerUrl, textColor}) => {
   return (
-    <div className="relative min-h-[180px] md:min-h-[170px] tablet:min-h-[200px] lg:min-h-[200px] lg:w-full">
+    <div className="relative h-[11.25rem] md:h-[14.625rem] tablet:h-[12.5rem] lg:h-[12rem] lg:w-full">
       <Image
-        className="object-cover w-full h-full"
+        className="object-fill w-full h-full"
         src={`${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${bannerUrl}`}
         alt={heading}
         priority
         width={800}
         height={200}
-        sizes="(max-width: 768px) 393px, (max-width: 1024px) 752px, 800px"
-        //@ts-ignore
-        srcSet={`
-            ${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${bannerUrl} 393w,
-            ${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${bannerUrl} 752w,
-            ${process.env.NEXT_PUBLIC_ASSETS_SERVER_URL}${bannerUrl} 800w
-    `}
       />
       <div
         className={`absolute font-century-gothic ${textColor === 'white' ? 'text-white' : 'text-[#3aa4dc]'} bottom-4 ml-6 mb-10 text-left font-normal`}
