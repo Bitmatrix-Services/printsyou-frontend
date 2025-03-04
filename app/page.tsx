68302e0ad137ac42a0d294d1de97004834edeb28
the-mgi <abdul.wahab394.aw@gmail.com>
2025-03-04 22:16:21 +0500

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,62 +2,61 @@ import HomeComponent from '@components/home/home-component';
 import {getAllCategories, getBannersList, getFaqsList, getProductsByTag} from '@components/home/home-apis';
 import React from 'react';
 
-export default async function HomePage() {
-  const [categoriesData, bannersList, faqsResponse, ...productsData] = await Promise.all([
-    getAllCategories(),
-    getBannersList(),
-    getFaqsList(),
-    getProductsByTag('newAndExclusive'),
-    getProductsByTag('under1Dollar'),
-    getProductsByTag('mostPopular'),
-    getProductsByTag('deals')
-  ]);
-
-  const sortedFaqs = [...(faqsResponse?.payload || [])].sort((a, b) => a.sequenceNumber - b.sequenceNumber);
-
-  const ldJsonData = [
-    {
-      '@context': 'https://schema.org',
-      '@type': 'Organization',
-      name: 'Prints You',
-      description:
-        'Discover top-quality promotional products. Perfect for trade shows, conventions or office swag. Elevate your brand with unique promotional products today!',
-      email: 'info@printsyou.com',
-      url: 'https://printsYou.com',
-      logo: 'https://printsyou.com/assets/logo-full.png',
-      telephone: '+1-888-299-2940',
-      address: {
-        '@type': 'PostalAddress',
-        streetAddress: '8602 Royal Star Rd',
-        addressLocality: 'Rowlett',
-        addressRegion: 'TX',
-        postalCode: '75089',
-        addressCountry: 'US'
-      },
-      sameAs: ['https://www.facebook.com/PrintsYouPromotional', 'https://www.linkedin.com/company/printsyou'],
-      contactPoint: {
-        '@type': 'ContactPoint',
-        telephone: '+1-888-299-2940',
-        contactOption: 'TollFree',
-        contactType: 'customer service',
-        areaServed: 'US',
-        availableLanguage: ['English'],
-        email: 'info@printsyou.com'
-      }
+const ldJsonData = [
+  {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'Prints You',
+    description:
+      'Discover top-quality promotional products. Perfect for trade shows, conventions or office swag. Elevate your brand with unique promotional products today!',
+    email: 'info@printsyou.com',
+    url: 'https://printsYou.com',
+    logo: 'https://printsyou.com/assets/logo-full.png',
+    telephone: '+1-888-299-2940',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: '8602 Royal Star Rd',
+      addressLocality: 'Rowlett',
+      addressRegion: 'TX',
+      postalCode: '75089',
+      addressCountry: 'US'
     },
-    {
-      '@context': 'https://schema.org',
-      '@type': 'WebSite',
-      url: 'https://www.printsyou.com/',
-      name: 'PrintsYou',
-      potentialAction: {
-        '@type': 'SearchAction',
-        target:
-          'https://printsyou.com/search-results?keywords={searchTerms}&filter=priceHighToLow&size=20&page=1&icid=gsearch',
-        'query-input': 'required name=searchTerms'
-      }
+    sameAs: ['https://www.facebook.com/PrintsYouPromotional', 'https://www.linkedin.com/company/printsyou'],
+    contactPoint: {
+      '@type': 'ContactPoint',
+      telephone: '+1-888-299-2940',
+      contactOption: 'TollFree',
+      contactType: 'customer service',
+      areaServed: 'US',
+      availableLanguage: ['English'],
+      email: 'info@printsyou.com'
     }
-  ];
+  },
+  {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    url: 'https://www.printsyou.com/',
+    name: 'PrintsYou',
+    potentialAction: {
+      '@type': 'SearchAction',
+      target:
+        'https://printsyou.com/search-results?keywords={searchTerms}&filter=priceHighToLow&size=20&page=1&icid=gsearch',
+      'query-input': 'required name=searchTerms'
+    }
+  }
+];
+
+export default async function HomePage() {
+  const categoriesData = await getAllCategories();
+  const newAndExclusiveData = await getProductsByTag('newAndExclusive');
+  const underABuck = await getProductsByTag('under1Dollar');
+  const innovativeIdea = await getProductsByTag('mostPopular');
+  const deals = await getProductsByTag('deals');
+  const bannersList = await getBannersList();
+
+  const response = await getFaqsList();
+
+  const sortedFaqs = [...(response?.payload || [])].sort((a, b) => a.sequenceNumber - b.sequenceNumber);
 
   const getFaqsSchema = () => ({
     '@context': 'https://schema.org',
@@ -91,10 +90,10 @@ export default async function HomePage() {
       <HomeComponent
         bannersList={bannersList.payload}
         categories={categoriesData.payload}
-        newAndExclusive={productsData[0]?.payload.content}
-        underABuck={productsData[1]?.payload.content}
-        innovativeIdea={productsData[2]?.payload.content}
-        deals={productsData[3]?.payload.content}
+        newAndExclusive={newAndExclusiveData.payload.content}
+        underABuck={underABuck.payload.content}
+        innovativeIdea={innovativeIdea.payload.content}
+        deals={deals.payload.content}
         faqsList={sortedFaqs}
       />
     </section>
